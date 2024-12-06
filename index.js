e7cd6aa936e2a9cf8f365d167d760f5e42994648
SHIVALEELAREDDY <itsshivureddy956@gmail.com>
2024-12-06 10:17:20 +0530

    index 2 files

---
+++ index.js
@@ -0,0 +1,52 @@
+if(!!"shivu"){
+    alert("hello there good morning");
+    
+}else{
+    alert("hello there see you tmrw")
+}
+
+if(10 && 10){
+    alert("hello there good morning");
+}else{
+    alert("hello there see you tmrm");
+}
+if(["shivu"]&&[1,2,3]){
+    alert("hello there good morning");
+}else{
+    alert("hello there see you tmrm");
+}
+if(["shivu"]==[1,2,3]){
+    alert("hello there good morning");
+}else{
+    alert("hello there see you tmrm!!!")
+}
+if(["shivu"]&&[]){
+    alert("hello there good morning");
+}else{
+    alert("hello there see you tmrm!!")
+}
+if(["shivu"]&& ""){
+    alert("hello there good morning");
+}else{
+    alert("hello there see you tmrm!!");
+}
+if(["shivu"]&& undefined){
+    alert("hello there good morning");
+}else{
+    alert("hello there see you tmrm!!");
+}
+if(null && null){
+    alert("hello there good morning");
+}else{
+    alert("hello there see you tmrm!!");
+}
+if("" || 10){
+    alert("hello there good morning");
+}else{
+    alert("hello there see you tmrm!!");
+}
+if(["shivu"]|| undefined){
+    alert("hello there good morning");
+}else{
+    alert("hello there see you tmrm!!");
+}
